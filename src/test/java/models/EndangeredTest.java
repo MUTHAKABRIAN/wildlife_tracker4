f06ffdaf6b808cdef130f3a464ef82bc4ac782c6
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-02 19:09:15 +0300

    test class for endangered animal

--- src/test/java/models/EndangeredTest.java
+++ src/test/java/models/EndangeredTest.java
@@ -1,7 +1,25 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
+
+    @Test
+    public void Animals_instantiatesName_of_the_animal_true_String(){
+        Endangered testEndangered = new Endangered("Lion", "Okay", "Newborn");
+        assertEquals("Lion", testEndangered.getName());
+    }
+
+
+    @Test
+    public void save_successfully_List() {
+        Endangered testEndangered = new Endangered("Lion", "Okay", "Newborn");;
+        testEndangered.save();
+        assertTrue(Endangered.getAllEndangered().get(0).equals(testEndangered));
+    }
+
+
 
 }
