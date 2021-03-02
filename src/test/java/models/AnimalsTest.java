51f7bf77667ecd546d0e5e989e9526dfc286182f
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-02 19:04:31 +0300

    add animal test class

---
+++ src/test/java/models/AnimalsTest.java
@@ -0,0 +1,28 @@
+package models;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import static org.junit.Assert.*;
+
+public class AnimalsTest {
+
+
+    @Test
+    public void Animals_instantiatesName_of_the_animal_true_String() {
+        Animals testAnimals = new Animals("Rhino");
+        assertEquals("Rhino", testAnimals.getAnimalName());
+    }
+
+
+    @Test
+    public void save_successfully_List() {
+        Animals testAnimals = new Animals("Lion");;
+        testAnimals.save();
+        assertTrue(Endangered.getAllEndangered().get(0).equals(testAnimals));
+    }
+
+
+
+}
