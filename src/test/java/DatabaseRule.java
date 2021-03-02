e6f3fc50e7bb947917acce4b435eb9e2524f4a1a
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-02 23:11:49 +0300

    add database rule

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
 
-public class DatabaseRule {
+import models.DB;
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", "moringa", "Nya2rango@");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+
+        }
+    }
+
 }
